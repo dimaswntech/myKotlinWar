481c0f42c31c9c62c594f514ee99c37826bc1055
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-05-07 09:54:41 +0700

    constantDT

--- src/main/kotlin/constantDT.kt
+++ src/main/kotlin/constantDT.kt
@@ -1,5 +1,5 @@
    fun main (args:Array<String>){
-       val number : Int=10 //Int: strores integer
+       val number : Int=11 //Int: strores integer
        val pi : Double = 3.14 //Double: stores decimal
        val k:Float=3.1212F //Float: end with F
 // contant same with const, that can be change in another call
